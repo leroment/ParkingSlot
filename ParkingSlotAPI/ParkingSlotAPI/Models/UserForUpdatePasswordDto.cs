d09e9860e1f3b95cc6dc3d48d32811c4b133f93b
leroment <andrewcai19972011@gmail.com>
2019-11-01 17:00:49 +0800

    reset password

--- ParkingSlotAPI/ParkingSlotAPI/Models/UserForUpdatePasswordDto.cs
+++ ParkingSlotAPI/ParkingSlotAPI/Models/UserForUpdatePasswordDto.cs
@@ -7,10 +7,13 @@ namespace ParkingSlotAPI.Models
 {
     public class UserForUpdatePasswordDto
     {
+        public Guid Id { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
+        public string Username { get; set; }
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
-        public string username { get; set; }
+
+  
     }
 }
