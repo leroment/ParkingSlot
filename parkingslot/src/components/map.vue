ec81aa990446821bc2fdbb86135c52b36722f498
Jiawei Tan <JiaweiTan1996@gmail.com>
2019-11-06 00:30:29 +0800

    Update map.vue

--- parkingslot/src/components/map.vue
+++ parkingslot/src/components/map.vue
@@ -1,127 +1,139 @@
 <template>
-    <gmap-map id="google-map" :center="center" ref="mapRef" :zoom="zoom" :options="mapStyle">
-        <gmap-marker :position="center" :icon="userMarkerOptions" @click="selectUserLocation()">
-            <gmap-info-window :position="center" @closeclick="currentinfo=false" :opened="currentinfo">
-                <h2>Current location</h2>
-                <v-container class>
-                    <v-row>
-                        <span class="body-1">Select a carpark to get more information and directions</span>
-                    </v-row>
-                </v-container>
-            </gmap-info-window>
-        </gmap-marker>
-        <gmap-info-window @closeclick="window_open=false"
-                          :opened="window_open"
-                          :position="markerPos"
-                          :options="{
+  <gmap-map id="google-map" :center="center" ref="mapRef" :zoom="zoom" :options="mapStyle">
+    <gmap-marker :position="center" :icon="userMarkerOptions" @click="selectUserLocation()">
+      <gmap-info-window :position="center" @closeclick="currentinfo=false" :opened="currentinfo">
+        <h2>Current location</h2>
+        <v-container class>
+          <v-row>
+            <span class="body-1">Select a carpark to get more information and directions</span>
+          </v-row>
+        </v-container>
+      </gmap-info-window>
+    </gmap-marker>
+    <gmap-info-window
+      @closeclick="window_open=false"
+      :opened="window_open"
+      :position="markerPos"
+      :options="{
               pixelOffset: {
                 width: 0,
                 height: -35
               }
-            }">
-            <h2>
-                {{ markerItem.carparkName }}
-                <v-chip v-if="markerItem.agencyType != '-1'"
-                        class="ma-2"
-                        color="primary"
-                        outlined>{{ markerItem.agencyType }}</v-chip>
-            </h2>
-            <v-container class>
-                <v-row v-if="markerItem.address != '-1'">
-                    <span class="body-1">Address: {{ markerItem.address }}</span>
-                </v-row>
-                <v-row v-if="markerItem.totalAvailableLots != '-1'">
-                    <span class="body-1">Total Available Lots: {{ markerItem.totalAvailableLots }}</span>
-                </v-row>
-                <v-row v-if="markerItem.totalLots != '-1'">
-                    <span class="body-1">Total Lots: {{ markerItem.totalLots }}</span>
-                </v-row>
-                <v-row v-if="markerItem.carAvailability != '-1'">
-                    <span class="body-1">Car Availability: {{ markerItem.carAvailability }}</span>
-                </v-row>
-                <v-row v-if="markerItem.mAvailability != '-1'">
-                    <span class="body-1">Motorcycle Availability: {{ markerItem.mAvailability }}</span>
-                </v-row>
-                <v-row v-if="markerItem.hvAvailability != '-1'">
-                    <span class="body-1">Heavy Vehicle Availability: {{ markerItem.hvAvailability }}</span>
-                </v-row>
-                <v-row v-if="markerItem.carCapacity != '-1'">
-                    <span class="body-1">Car Capacity: {{ markerItem.carCapacity }}</span>
-                </v-row>
-                <v-row v-if="markerItem.mCapacity != '-1'">
-                    <span class="body-1">Motorcycle Capacity: {{ markerItem.mCapacity }}</span>
-                </v-row>
-                <v-row v-if="markerItem.hvCapacity != '-1'">
-                    <span class="body-1">Heavy Vehicle Capacity: {{ markerItem.hvCapacity }}</span>
-                </v-row>
-            </v-container>-->
-            <v-btn class="mt-5" color="info" @click="getDirection(markerPos)" type="submit">Directions</v-btn>
-        </gmap-info-window>
-    <!--     <CarparkFilter class="filterBtn" id="filter" @clicked="onFilter"></CarparkFilter>
-        <v-toolbar id="filterBar" dense floating>
-            <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
-            <v-btn icon>
-                <v-icon color="blue">mdi-crosshairs-gps</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>mdi-dots-horizontal</v-icon>
-            </v-btn>
-        </v-toolbar>-->
-        <template>
-            <div class="text-center">
-                <v-dialog v-model="sheet" height="90%" width="500px">
-                    <template v-slot:activator="{ on }">
-                        <v-btn color="purple"
-                               v-show="route_open"
-                               id="routeModal"
-                               style="top:92%;"
-                               dark
-                               v-on="on">Show Route</v-btn>
-                    </template>
+            }"
+    >
+      <h2>
+        {{ markerItem.carparkName }}
+        <v-chip
+          v-if="markerItem.agencyType != '-1'"
+          class="ma-2"
+          color="primary"
+          outlined
+        >{{ markerItem.agencyType }}</v-chip>
+      </h2>
+      <v-container class>
+        <v-row v-if="markerItem.address != '-1'">
+          <span class="body-1">Address: {{ markerItem.address }}</span>
+        </v-row>
+        <v-row v-if="markerItem.totalAvailableLots != '-1'">
+          <span class="body-1">Total Available Lots: {{ markerItem.totalAvailableLots }}</span>
+        </v-row>
+        <v-row v-if="markerItem.totalLots != '-1'">
+          <span class="body-1">Total Lots: {{ markerItem.totalLots }}</span>
+        </v-row>
+        <v-row v-if="markerItem.carAvailability != '-1'">
+          <span class="body-1">Car Availability: {{ markerItem.carAvailability }}</span>
+        </v-row>
+        <v-row v-if="markerItem.mAvailability != '-1'">
+          <span class="body-1">Motorcycle Availability: {{ markerItem.mAvailability }}</span>
+        </v-row>
+        <v-row v-if="markerItem.hvAvailability != '-1'">
+          <span class="body-1">Heavy Vehicle Availability: {{ markerItem.hvAvailability }}</span>
+        </v-row>
+        <v-row v-if="markerItem.carCapacity != '-1'">
+          <span class="body-1">Car Capacity: {{ markerItem.carCapacity }}</span>
+        </v-row>
+        <v-row v-if="markerItem.mCapacity != '-1'">
+          <span class="body-1">Motorcycle Capacity: {{ markerItem.mCapacity }}</span>
+        </v-row>
+        <v-row v-if="markerItem.hvCapacity != '-1'">
+          <span class="body-1">Heavy Vehicle Capacity: {{ markerItem.hvCapacity }}</span>
+        </v-row>
+      </v-container>-->
+      <v-btn class="mt-5" color="info" @click="getDirection(markerPos)" type="submit">Directions</v-btn>
+    </gmap-info-window>
+    <!--<CarparkFilter class="filterBtn" id="filter" @clicked="onFilter"></CarparkFilter>-->
+    <v-toolbar id="filterBar" dense floating>
+      <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
+      <v-btn icon>
+        <v-icon color="blue">mdi-crosshairs-gps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-horizontal</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <template>
+      <div class="text-center">
+        <v-dialog v-model="sheet" height="90%" width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn
+              color="purple"
+              v-show="route_open"
+              id="routeModal"
+              style="top:92%;"
+              dark
+              v-on="on"
+            >Show Route</v-btn>
+          </template>
 
-                    <v-card scrollable>
-                        <v-card style="border-radius:0;" class="mx-auto mb-5" color="#26c6da" dark>
-                            <v-card-title>
-                                <v-icon large left>mdi-routes</v-icon>
-                                <span class="title font-weight-light">Routing Information</span>
-                            </v-card-title>
-                            <v-card-text class="headline py-1 font-weight-regular">Start:{{routeInfo.startaddress}}</v-card-text>
-                            <v-card-text class="headline py-1 font-weight-regular">End: {{routeInfo.endaddress}}</v-card-text>
-                            <v-card-text class="headline py-1 font-weight-regular">Total Distance:{{routeInfo.distance}}</v-card-text>
-                            <v-card-text class="headline py-1 mb-5 font-weight-regular">Total Duration:{{routeInfo.duration}}</v-card-text>
-                        </v-card>
-                        <template v-for="(item, index) in steps">
-                            <v-card-text :key="index" class="ml-4">
-                                <v-col py-1>
-                                    <v-badge color="orange" left>
-                                        <template v-slot:badge>
-                                            <span>{{index+1}}</span>
-                                        </template>
-                                    </v-badge>
-                                </v-col>
-                                <v-row>
-                                    <span class="body-1">Distance: {{item.distance.text}}</span>
-                                </v-row>
-                                <v-row>
-                                    <span class="body-1">Duration: {{item.duration.text}}</span>
-                                </v-row>
-                                <v-row>
-                                    <span class="body-1">Instructions:</span>
-                                </v-row>
-                                <v-row>
-                                    <span class="body-1" v-html="item.instructions"></span>
-                                </v-row>
-                                <v-divider></v-divider>
-                            </v-card-text>
-                        </template>
-                    </v-card>
-                </v-dialog>
-            </div>
-        </template>
-        <v-btn id="gpsBtn" @click="geolocation" class="ma-2 gmcontrol1">
-            <v-icon size="28" color="blue darken-1">mdi-crosshairs-gps</v-icon>
-        </v-btn>
-    </gmap-map>
+          <v-card scrollable>
+            <v-card style="border-radius:0;" class="mx-auto mb-5" color="#26c6da" dark>
+              <v-card-title>
+                <v-icon large left>mdi-routes</v-icon>
+                <span class="title font-weight-light">Routing Information</span>
+              </v-card-title>
+              <v-card-text
+                class="headline py-1 font-weight-regular"
+              >Start:{{routeInfo.startaddress}}</v-card-text>
+              <v-card-text class="headline py-1 font-weight-regular">End: {{routeInfo.endaddress}}</v-card-text>
+              <v-card-text
+                class="headline py-1 font-weight-regular"
+              >Total Distance:{{routeInfo.distance}}</v-card-text>
+              <v-card-text
+                class="headline py-1 mb-5 font-weight-regular"
+              >Total Duration:{{routeInfo.duration}}</v-card-text>
+            </v-card>
+            <template v-for="(item, index) in steps">
+              <v-card-text :key="index" class="ml-4">
+                <v-col py-1>
+                  <v-badge color="orange" left>
+                    <template v-slot:badge>
+                      <span>{{index+1}}</span>
+                    </template>
+                  </v-badge>
+                </v-col>
+                <v-row>
+                  <span class="body-1">Distance: {{item.distance.text}}</span>
+                </v-row>
+                <v-row>
+                  <span class="body-1">Duration: {{item.duration.text}}</span>
+                </v-row>
+                <v-row>
+                  <span class="body-1">Instructions:</span>
+                </v-row>
+                <v-row>
+                  <span class="body-1" v-html="item.instructions"></span>
+                </v-row>
+                <v-divider></v-divider>
+              </v-card-text>
+            </template>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
+    <v-btn id="gpsBtn" @click="geolocation" class="ma-2 gmcontrol1">
+      <v-icon size="28" color="blue darken-1">mdi-crosshairs-gps</v-icon>
+    </v-btn>
+  </gmap-map>
 </template>
 
 <script>
@@ -199,20 +211,20 @@ export default {
       //Filter by the markers
       this.filterConfig = filterConfig;
     },
-  initGmaps: function() {
-     /*   var filterBar = document.getElementById("filter");
+    initGmaps: function() {
+      /*   var filterBar = document.getElementById("filter"); */
       var gpsBtn = document.getElementById("gpsBtn");
       var routeModal = document.getElementById("routeModal");
       this.mapObject = this.$refs.mapRef.$mapObject;
-      this.mapObject.controls[google.maps.ControlPosition.TOP_RIGHT].push(
+      /*this.mapObject.controls[google.maps.ControlPosition.TOP_RIGHT].push(
         filterBar
-      );
+      );*/
       this.mapObject.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
         gpsBtn
       );
       this.mapObject.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(
         routeModal
-      );*/
+      );
       //To change the polyline color
       //polylineOptions:{strokeColor:"#4a4a4a",strokeWeight:5},
       directionsDisplay = new google.maps.DirectionsRenderer({
